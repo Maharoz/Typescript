bc0e17a0e0cc516b8de6e2b393dec5095c782d8f
Maharoz <mahruj66@gmail.com>
2021-01-25 20:48:55 +0600

    Added Object Types

--- app.ts
+++ app.ts
@@ -4,17 +4,7 @@
 // } = {
     const person = {
         name: 'Maximilian',
-        age: 30,
-        hobbies: ['Sports', 'Cooking']
+        age: 30
       };
       
-      let favoriteActivities: string[];
-      favoriteActivities = ['Sports'];
-      
       console.log(person.name);
-      
-      for (const hobby of person.hobbies) {
-        console.log(hobby.toUpperCase());
-        // console.log(hobby.map()); // !!! ERROR !!!
-      }
-      
