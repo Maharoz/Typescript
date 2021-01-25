b7e00c5be1a6238072f71d6074fdd18cb7d4c264
Maharoz <mahruj66@gmail.com>
2021-01-25 20:43:23 +0600

    Types script type s javascript types

--- app.ts
+++ app.ts
@@ -1,4 +1,7 @@
 function add(n1: number, n2: number) {
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //   throw new Error('Incorrect input!');
+    // }
     return n1 + n2;
   }
   
@@ -7,3 +10,4 @@ function add(n1: number, n2: number) {
   
   const result = add(number1, number2);
   console.log(result);
+  
