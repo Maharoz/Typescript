5c1ae66aad2b68f8c3c87605d595220218b5c97a
Maharoz <mahruj66@gmail.com>
2021-01-25 21:01:01 +0600

    enum implemented

--- app.ts
+++ app.ts
@@ -2,27 +2,29 @@
 //   name: string;
 //   age: number;
 // } = {
-    const person:{
-        name:string;
-        age:number;
-        hobbies:string[];
-        role:[number,string]
-    } = {
+
+enum Role {ADMIN=5,READ_ONLY=100,AUTHOR=200};
+
+    const person= {
         name: 'Maximilian',
         age: 30,
         hobbies: ['Sports', 'Cooking'],
-        role:[2,'author']
+        role:Role.ADMIN
       };
 
-      person.role.push('admin');
-     // person.role[1]=10;
+    //   person.role.push('admin');
+    //  // person.role[1]=10;
       
-      let favoriteActivities: string[];
-      favoriteActivities = ['Sports'];
+    //   let favoriteActivities: string[];
+    //   favoriteActivities = ['Sports'];
       
       console.log(person.name);
       
       for (const hobby of person.hobbies) {
         console.log(hobby.toUpperCase());
         // console.log(hobby.map()); // !!! ERROR !!!
+      }
+
+      if(person.role== Role.AUTHOR){
+          console.log('is author');
       }
