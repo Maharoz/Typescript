77252dbcdead323e8f0f0cc176be14ec732677b7
Maharoz <mahruj66@gmail.com>
2021-01-29 20:45:03 +0600

    Custom types implemented

--- app.ts
+++ app.ts
@@ -1,13 +1,19 @@
+type Combineable =number |string;
+type CoversionDescriptor = "as-number" | "as-text";
 function combine(
-  input1:number|string,
-  input2:number|string,
-  resultConversion:'as-number' | 'as-text'){
+  input1: Combineable ,
+  input2: Combineable ,
+  resultConversion: CoversionDescriptor
+) {
   let result;
 
-  if(typeof input1 ==='number' && typeof input2==='number'|| resultConversion === 'as-number'){
-     result = +input1 + +input2;
-  }else{
-    result= input1.toString() +input2.toString();
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
   }
   return result;
 
@@ -16,17 +22,15 @@ function combine(
   // }else{
   //   return result.toString();
   // }
- 
 }
 
-
-const combinedAges = combine(30,26,'as-number');
+const combinedAges = combine(30, 26, "as-number");
 
 console.log(combinedAges);
 
-const combinedStringAges = combine(30,26,'as-number');
+const combinedStringAges = combine(30, 26, "as-number");
 console.log(combinedStringAges);
 
-const combinedNames = combine('Max','Anna','as-text');
+const combinedNames = combine("Max", "Anna", "as-text");
 
 console.log(combinedNames);
