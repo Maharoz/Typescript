55a4ad287745ce5978ceb90a09899f962c7415f5
Maharoz <mahruj66@gmail.com>
2022-10-21 20:50:32 +0600

    Private and public

--- ClassAndInterface/src/app.js
+++ ClassAndInterface/src/app.js
@@ -1,12 +1,25 @@
 //Creating a class inn type script
 var Department = /** @class */ (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log("department" + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department("Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 accounting.describe();
+accounting.printEmployeeInformation();
+var accountingCopy = { name: "s", describe: accounting.describe };
+//accountingCopy.describe();
--- ClassAndInterface/src/app.ts
+++ ClassAndInterface/src/app.ts
@@ -1,6 +1,7 @@
 //Creating a class inn type script
 class Department {
   name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -9,10 +10,21 @@ class Department {
   describe(this: Department) {
     console.log("department" + this.name);
   }
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 accounting.describe();
+accounting.printEmployeeInformation();
 const accountingCopy = { name: "s", describe: accounting.describe };
-accountingCopy.describe();
+//accountingCopy.describe();
