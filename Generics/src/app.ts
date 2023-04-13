3d8e6b8295a52d064f15180cd03f6de7286cb58d
Maharoz <mahruj66@gmail.com>
2023-04-13 20:58:29 +0600

    key of constrains

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -14,8 +14,25 @@
 // });
 
 function merge<T extends Object, U extends Object>(objA: T, objB: U) {
-  return Object.assign( objA, objB);
+  return Object.assign(objA, objB);
 }
 
 const mergedObj = merge({ name: "Max" }, { age: 30 });
 //mergedObj.age;
+
+interface Lengthy {
+  length: number;
+}
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " element";
+  }
+  return [element, descriptionText];
+}
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return "Value" + obj[key];
+}
