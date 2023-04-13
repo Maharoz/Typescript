36d308e7788bdfc38643a9c2bf628da51c9d3d9b
Maharoz <mahruj66@gmail.com>
2023-04-13 20:29:48 +0600

    built in generics

---
+++ Generics/src/app.ts
@@ -0,0 +1,14 @@
+// Code goes here!
+//const names=['Max','Manuel'];
+// const names: Array<string> = [];
+// //names[0].split(" ");
+
+// const promise = new Promise<any>((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("This is done!!");
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   data.split(" ");
+// });
