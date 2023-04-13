c2927272a413f1d279a12ae0b06b1202a7ee49fe
Maharoz <mahruj66@gmail.com>
2023-04-13 20:45:15 +0600

    generics with constrains

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -12,3 +12,10 @@
 // promise.then((data) => {
 //   data.split(" ");
 // });
+
+function merge<T extends Object, U extends Object>(objA: T, objB: U) {
+  return Object.assign( objA, objB);
+}
+
+const mergedObj = merge({ name: "Max" }, { age: 30 });
+//mergedObj.age;
